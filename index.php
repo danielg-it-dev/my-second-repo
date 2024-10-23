3b76266782384c6dc13d64cbb9cc9f1f21ba5de3
danielg-it-dev <danielgheorghitacareeroppsuk@gmail.com>
2024-10-23 15:02:24 +0100

    Adding project to GitHub repo

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My modified file</title>
+</head>
+<body>
+  <div>
+    <p>I am <?php echo "31" ?> years old</p>
+  </div>
+</body>
+</html>
